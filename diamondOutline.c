a1ead91a00477cd641bead1d873b4b745a1da08a
Akash <88789033+whoakashpatel@users.noreply.github.com>
2023-10-20 14:28:53 +0530

    Add comments

--- diamondOutline.c
+++ diamondOutline.c
@@ -3,16 +3,22 @@
 int main() {
     int n,i,j;
     scanf("%d", &n);
+    // for upper part of the diamond
     for(i=0;i<n;i++) {
+        // for indentation
         for(j=n;j>i+1;j--) printf(" ");
+        // for count and print
         for(j=0;j<=i;j++) {
             if(j==0||j==i) printf("* ");
             else printf("  ");
         }
         printf("\n");
     }
+    // for inverted part of the diamond
     for(;i>1;i--) {
+        // for indentation
         for(j=n;j>i-1;j--) printf(" ");
+        // for count and print
         for(j=0;j<=i-2;j++) {
             if(j==0||j==i-2) printf("* ");
             else printf("  ");
