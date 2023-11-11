9aa68b1371d321221b7b6a89bb65801da7f5c60a
Akash <akash.patel9690@gmail.com>
2023-11-11 19:00:09 +0530

    Employ 2DArr instead of Struct for storing values

--- POTD/nQueries1.c
+++ POTD/nQueries1.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 int n;
-void sort(int arr[], int index[]);
-void removeR(int arr[], int index[], int r);
+void sort(int queries[][n], int index[][n], int q);
+void shiftR(int queries[][n], int index[][n], int q, int r);
 
 int main() {
     scanf("%d", &n);
     // initialise array of arrays using structure
-    struct program {
-        int queries[n];
-        int index[n];
-    } list[n];
+    int queries[n][n], index[n][n];
     // take input programs and queries
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {
-            scanf("%d", &list[i].queries[j]);
-            list[i].index[j]=j;
+            scanf("%d", &queries[i][j]);
+            index[i][j]=j;
         }
     }
     // take input indices to remove
@@ -23,38 +20,38 @@ int main() {
     for(int i=0;i<n-1;i++) scanf("%d", &rmIndex[i]);
 
     // sort each query & indice array
-    for(int i=0;i<n;i++) sort(list[i].queries, list[i].index);
+    for(int i=0;i<n;i++) sort(queries, index, i);
     // remove the rth min(s)
     for(int i=0;i<n;i++) {
         for(int j=0;j<n-1;j++) {
-            removeR(list[i].queries, list[i].index, rmIndex[j]-1);
+            shiftR(queries, index, i, rmIndex[j]-1);
         }
     }
     // print the remaining element
-    for(int i=0;i<n;i++) printf("%d %d\n", list[i].queries[0], list[i].index[0]);
+    for(int i=0;i<n;i++) printf("%d %d\n", queries[i][0], index[i][0]);
     return 0;
 }
 
-void sort(int arr[], int index[]) {
+void sort(int queries[][n], int index[][n], int q) {
     for(int i=0;i<n-1;i++) {
         for(int j=0;j<n-1;j++) {
-            if(arr[j]>arr[j+1]) {
+            if(queries[q][j]>queries[q][j+1]) {
                 // switch elements
-                arr[j]^=arr[j+1];
-                arr[j+1]^=arr[j];
-                arr[j]^=arr[j+1];
+                queries[q][j]^=queries[q][j+1];
+                queries[q][j+1]^=queries[q][j];
+                queries[q][j]^=queries[q][j+1];
                 // switch indices
-                index[j]^=index[j+1];
-                index[j+1]^=index[j];
-                index[j]^=index[j+1];
+                index[q][j]^=index[q][j+1];
+                index[q][j+1]^=index[q][j];
+                index[q][j]^=index[q][j+1];
             }
         }
     }
 }
 
-void removeR(int arr[], int index[], int r) {
+void shiftR(int queries[][n], int index[][n], int q, int r) {
     for(int i=r;i<n-1;i++) {
-        arr[i]=arr[i+1];
-        index[i]=index[i+1];
+        queries[q][i]=queries[q][i+1];
+        index[q][i]=index[q][i+1];
     }
 }
